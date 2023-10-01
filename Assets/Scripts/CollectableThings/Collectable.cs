35efa45eaeabb99a774ebb43773eeec6ee7b91ad
Chengzi-creator <1041379828@qq.com>
2023-10-02 00:49:27 +0800

    10.1
    今天大致实现了人物的八方移动，血量和移速的设定，完成了部分动画。设置了部分可拾取物体。试图写出敌人行动的AI

---
+++ Assets/Scripts/CollectableThings/Collectable.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//掉落物的拾取脚本
+public class Collectable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //碰撞检测相关
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerController pc = other.GetComponent<PlayerController>();
+        if (pc != null)
+        {
+            if (pc.MyCurrentHealth < pc.MyMaxHealth)
+            {
+                pc.ChangeHealth(1);
+                Destroy(this.gameObject);
+            }
+        }
+        //恢复生命，即红心
+        // if (other.CompareTag("Key"))
+        // {
+        //     if (pc != null)
+        //     {
+        //         if (pc.qualify < 5)
+        //         {
+        //             pc.ChangeHealth(1);
+        //             Destroy(this.gameObject);
+        //         }
+        //     }
+        // }//获得前往下一关的资格
+    }
+}
